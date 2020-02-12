1aa92f4fe0333716bd69a2ee92eaaee1d5ac72f7
Pedro Machado <pedro.machado@tala.co>
2020-02-12 13:24:14 -0600

    bdd statatements modification

--- src/test/java/net/me/dev/app/bdd/EmployeeControllerTest.groovy
+++ src/test/java/net/me/dev/app/bdd/EmployeeControllerTest.groovy
@@ -19,11 +19,11 @@ class EmployeeControllerTest extends Specification{
     }
 
     def "verify findAll"() {
-        given: "expected result is set"
+        given: "expected result is mocked"
             expected_result = EmployeeFacade.buildEmployeeList()
             // >> its the mock operator it means when you call findAll return expected_result object
             repository.findAll() >> expected_result
-        when: "actual result is retrieved"
+        when: "actual result is retrieved from controller"
             actual_result = controller.findAll()
         then: "actual result is compared to expected result"
             assert actual_result.statusCode == HttpStatus.OK
@@ -31,12 +31,12 @@ class EmployeeControllerTest extends Specification{
     }
 
     def "verify findById"() {
-        given: "expected result is set"
+        given: "expected result is mocked"
             expected_result = EmployeeFacade.buildEmployee()
 
             // >> its the mock operator it means when you call findAll return expected_result object
             repository.findById(expected_result.id) >> Optional.of(expected_result)
-        when: "actual data is retrieved"
+        when: "actual data is retrieved from controller"
             actual_result = controller.findById(expected_result.id)
         then: "actual result is compared to expected result"
             assert actual_result.statusCode == HttpStatus.OK
@@ -44,12 +44,12 @@ class EmployeeControllerTest extends Specification{
     }
 
     def "verify findByFirstName"() {
-        given: "expected result is set"
+        given: "expected result is mocked"
             expected_result = EmployeeFacade.buildEmployeeList()
 
             // >> its the mock operator it means when you call findAll return expected_result object
             repository.findByFirstNameIgnoreCase(expected_result.first().firstName) >> expected_result
-        when: "actual result is retrieved"
+        when: "actual result is retrieved from controller"
             actual_result = controller.findByFirstName(expected_result.first().firstName)
         then: "actual result is compared to expected result"
         verifyAll(actual_result) {
@@ -59,12 +59,12 @@ class EmployeeControllerTest extends Specification{
     }
 
     def "verify findByStatus"() {
-        given: "expected result is set"
+        given: "expected result is mocked"
             expected_result = EmployeeFacade.buildEmployeeList()
 
             // >> its the mock operator it means when you call findAll return expected_result object
             repository.findByStatus(expected_result.first().status) >> expected_result
-        when: "actual result is retrieved"
+        when: "actual result is retrieved from controller"
             actual_result = controller.findByStatus(expected_result.first().status)
         then: "actual result is compared to expected result"
             verifyAll(actual_result) {
@@ -74,12 +74,12 @@ class EmployeeControllerTest extends Specification{
     }
 
     def "verify createEntry"() {
-        given: "expected result is set"
+        given: "expected result is mocked"
             expected_result = EmployeeFacade.buildEmployee()
 
             // >> its the mock operator it means when you call findAll return expected_result object
             repository.save(expected_result) >> expected_result
-        when: "actual result is retrieved"
+        when: "actual result is retrieved from controller"
             actual_result = controller.createEntry(expected_result)
         then: "actual result is compared to expected result"
             verifyAll(actual_result) {
@@ -89,14 +89,14 @@ class EmployeeControllerTest extends Specification{
     }
 
     def "verify modifyEntry"() {
-        given: "expected result is set"
+        given: "expected result is mocked"
             expected_result = EmployeeFacade.buildEmployee()
             expected_result.firstName = "Test LastName Modified"
 
             //findById and save were mocked since modifyEntry is using both methods
             repository.findById(expected_result.id) >> Optional.of(expected_result)
             repository.save(expected_result) >> expected_result
-        when: "actual result is retrieved"
+        when: "actual result is retrieved from controller"
             actual_result = controller.modifyEntry(expected_result.id, expected_result)
         then: "actual result is compared to expected result"
             verifyAll(actual_result) {
@@ -106,10 +106,12 @@ class EmployeeControllerTest extends Specification{
     }
 
     def "verify deleteEntry"() {
-        given: "expected result is set"
+        given: "expected result is mocked"
             expected_result = EmployeeFacade.buildEmployee()
+
+            // >> its the mock operator it means when you call findAll return expected_result object
             repository.findById(expected_result.id) >> Optional.of(expected_result)
-        when: "actual result is retrieved"
+        when: "actual result is retrieved from controller "
             actual_result = controller.deleteEntry(expected_result.id)
         then: "actual result is compared to expected result"
             verifyAll(actual_result) {
